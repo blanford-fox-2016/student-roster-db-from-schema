f429565f75021d0ab5fe0aadac7cc616d7170853
Juang Wiantoro <juangwiantoro@gmail.com>
2016-10-18 00:44:14 +0700

    adding repl

--- student_roster.js
+++ student_roster.js
@@ -27,7 +27,7 @@ class Student {
     });
   }
 
-  deleteData(id) {
+  static deleteData(id) {
     db.run(`DELETE FROM student WHERE id = '${id}';`, (err) => {
       if (err) {
         console.error(err);
@@ -103,47 +103,103 @@ class Student {
 
 
 var example = [{
-  firstname : "aji",
+  firstname: "aji",
   lastname: "lantang",
   birthdate: "28-02-1990",
   sex: "Male"
-},
-{
-  firstname : "juang",
+}, {
+  firstname: "juang",
   lastname: "wiantoro",
   birthdate: "03-02-1990",
   sex: "Male"
-},
-{
-  firstname : "mangku",
+}, {
+  firstname: "mangku",
   lastname: "widodo",
   birthdate: "01-01-1990",
   sex: "Male"
-},
-{
-  firstname : "ken",
+}, {
+  firstname: "ken",
   lastname: "arok",
   birthdate: "04-04-1990",
   sex: "Male"
-},
-{
-  firstname : "Dharmadi",
+}, {
+  firstname: "Dharmadi",
   lastname: "Yea",
   birthdate: "05-05-1990",
   sex: "Male"
 }];
 
 
-
 let hacktiv8 = new Student(example[1]);
-hacktiv8.addData();
-hacktiv8.showData();
-hacktiv8.deleteData(1);
-hacktiv8.addData(example[2]);
-hacktiv8.showData();
-hacktiv8.addData(example[3]);
-hacktiv8.showData_firstname('Riza');
-hacktiv8.shortbyBirth();
-hacktiv8.showData(2);
-hacktiv8.birthThisMonth("02");
-hacktiv8.shortbyBirth();
+
+
+
+//// Added repl
+
+var replServer = repl.start({
+  prompt: 'show, add[num], birthday[month], firstname[name]> '
+});
+replServer.defineCommand('add', {
+  help: 'create',
+  action: function (num) {
+    console.log('I reserved some data, please just put the number from 0 to 4');
+    hacktiv8.addData(example[num]);
+    this.lineParser.reset();
+    this.bufferedCommand = '';
+    //hacktiv8.showData();
+    //console.log(`Hello, ${hacktiv8}`);
+    this.displayPrompt();
+  }
+});
+replServer.defineCommand('show', {
+  help: 'create',
+  action: function () {
+    console.log('Our database is represented by this');
+    this.lineParser.reset();
+    this.bufferedCommand = '';
+    hacktiv8.showData();
+    //console.log(`Hello, ${hacktiv8}`);
+    this.displayPrompt();
+  }
+});
+replServer.defineCommand('birthday', {
+  help: 'create',
+  action: function (num) {
+    console.log('Check if someone birthday or not, put the month');
+    hacktiv8.birthThisMonth(num)
+    this.lineParser.reset();
+    this.bufferedCommand = '';
+    //hacktiv8.showData();
+    //console.log(`Hello, ${hacktiv8}`);
+    this.displayPrompt();
+  }
+});
+replServer.defineCommand('firstname', {
+  help: 'create',
+  action: function (name) {
+    console.log('Check if someone birthday or not, put the month');
+    hacktiv8.showData_firstname(name)
+    this.lineParser.reset();
+    this.bufferedCommand = '';
+    //hacktiv8.showData();
+    //console.log(`Hello, ${hacktiv8}`);
+    this.displayPrompt();
+  }
+});
+replServer.defineCommand('saybye', function () {
+  console.log('Goodbye!');
+  this.close();
+});
+
+
+// hacktiv8.addData();
+// hacktiv8.showData();
+// hacktiv8.deleteData(1);
+// hacktiv8.addData(example[2]);
+// hacktiv8.showData();
+// hacktiv8.addData(example[3]);
+// hacktiv8.showData_firstname('Riza');
+// hacktiv8.shortbyBirth();
+// hacktiv8.showData(2);
+// hacktiv8.birthThisMonth("02");
+// hacktiv8.shortbyBirth();
